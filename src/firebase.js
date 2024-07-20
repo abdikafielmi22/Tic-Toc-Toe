92c7b21e5369d057442017e2df31a66e1979027d
abdikafielmi24 <abdikafielmi24@gmail.com>
2024-07-20 23:48:00 +0300

    firebase component

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+// Import necessary functions from the Firebase SDK
+import { initializeApp } from 'firebase/app'; // Import initializeApp to initialize Firebase
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'; // Import getAuth and GoogleAuthProvider for authentication
 
+// Firebase configuration object containing API keys and identifiers
 const firebaseConfig = {
     apiKey: "AIzaSyDq-ZwKtnS9NODalDcq-1SFDl13ySopeC0",
     authDomain: "tic-toc-toe-7c569.firebaseapp.com",
@@ -10,8 +12,12 @@ const firebaseConfig = {
     appId: "1:257626800006:web:68193aecef6ce53656b607"
 };
 
+// Initialize Firebase app with the configuration
 const app = initializeApp(firebaseConfig);
+// Get the authentication instance from the initialized app
 const auth = getAuth(app);
+// Create a new instance of the GoogleAuthProvider
 const provider = new GoogleAuthProvider();
 
+// Export the auth and provider instances for use in other parts of the application
 export { auth, provider };
